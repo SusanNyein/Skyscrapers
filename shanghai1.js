a5ee4704ad82169547ec8099ef6ba924b0d0cbaf
Susan <52238580+SusanNyein@users.noreply.github.com>
2021-10-21 14:15:36 +0630

    Add files via upload

---
+++ shanghai1.js
@@ -0,0 +1,10 @@
+var go1=document.getElementById("go");
+var webpage2="shanghaidiscourse.html";
+go1.onclick=function(){
+window.location=webpage2;}
+
+var photo1=document.getElementById("photo");
+var webpage22="shanghaiphoto.html";
+photo1.onclick=function(){
+window.location=webpage22;}
+
